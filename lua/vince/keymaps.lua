6ebdf2e99d1d6ee5c6d0e91cef3cc3bee3eddd5e
vince liem <vincepaul.liem@gmail.com>
2026-03-13 15:45:49 +0100

    Add keymap to copy current file path

--- lua/vince/keymaps.lua
+++ lua/vince/keymaps.lua
@@ -2,6 +2,7 @@ local map = vim.keymap.set
 
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 map('n', '<leader>w', '<cmd>write<CR>', { desc = 'Write buffer' })
+vim.keymap.set('n', '<leader>fp', function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = 'Copy file path' })
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Diagnostics to location list' })
 map('n', 'gl', vim.diagnostic.open_float, { desc = 'Line diagnostics' })
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
